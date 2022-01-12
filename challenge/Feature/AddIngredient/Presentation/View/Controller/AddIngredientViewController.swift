ed07cacdcce37d94d74ba215456c366663d7032c
Mohammed Eltaweel <31571400+mohamedmagdy94@users.noreply.github.com>
2022-01-12 19:52:34 +0200

    Add Ingredient View

--- challenge/Feature/AddIngredient/Presentation/View/Controller/AddIngredientViewController.swift
+++ challenge/Feature/AddIngredient/Presentation/View/Controller/AddIngredientViewController.swift
@@ -15,7 +15,8 @@ class AddIngredientViewController: UIViewController {
     @IBOutlet weak var userInput: UITextView!
     @IBOutlet weak var analyzeButton: UIButton!
     
-    private var viewModel: AddIngredientView
+    private var viewModel: AddIngredientViewModelProtocol?
+    private var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,7 +24,39 @@ class AddIngredientViewController: UIViewController {
     }
     
     private func setup(){
-        
+        setupViews()
+        setupRx()
+    }
+    
+    private func setupViews(){
+        userInput.layer.borderWidth = 1
+        userInput.layer.borderColor = UIColor.gray.cgColor
+        userInput.layer.cornerRadius = 8
+    }
+    
+    private func setupRx(){
+        guard let viewModel = viewModel else{ return }
+        viewModel
+            .isSubmitAvailable
+            .bind(to: analyzeButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+        userInput
+            .rx
+            .text
+            .filterNil()
+            .asObservable()
+            .bind(to: viewModel.userIngredients)
+            .disposed(by: disposeBag)
+        analyzeButton
+            .rx
+            .tap
+            .asObservable()
+            .bind(to: viewModel.submit)
+            .disposed(by: disposeBag)
+    }
+    
+    func setViewModel(viewModel: AddIngredientViewModelProtocol){
+        self.viewModel = viewModel
     }
     
 }
